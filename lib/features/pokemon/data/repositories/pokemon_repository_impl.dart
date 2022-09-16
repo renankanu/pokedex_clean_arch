c6e5035b57de8ad6810806d3e7f6be4cfc978e0f
renankanu <renankanu@gmail.com>
2022-09-16 19:59:32 -0300

    feature: init teste repository

--- lib/features/pokemon/data/repositories/pokemon_repository_impl.dart
+++ lib/features/pokemon/data/repositories/pokemon_repository_impl.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:pokedex_clean_arch/core/errors/failure.dart';
 import 'package:pokedex_clean_arch/features/pokemon/domain/entities/pokemon.dart';
 import 'package:pokedex_clean_arch/features/pokemon/domain/repositories/pokemon_repository.dart';
 
+import '../../../../core/errors/exception.dart';
 import '../datasources/remote_data_source.dart';
 
 class PokemonRepositoryImpl implements PokemonRepository {
@@ -12,11 +15,17 @@ class PokemonRepositoryImpl implements PokemonRepository {
 
   @override
   Future<Either<Failure, List<Pokemon>>> getAllPokemons() async {
-    final result = await remoteDataSource.getAllPokemons();
-    final List<Pokemon> listPokemon = [];
-    for (var pokemonModel in result) {
-      listPokemon.add(pokemonModel.toEntity());
+    try {
+      final result = await remoteDataSource.getAllPokemons();
+      final List<Pokemon> listPokemon = [];
+      for (var pokemonModel in result) {
+        listPokemon.add(pokemonModel.toEntity());
+      }
+      return Right(listPokemon);
+    } on ServerException {
+      return const Left(ServerFailure(''));
+    } on SocketException {
+      return const Left(ConnectionFailure('Failed to connect to the network'));
     }
-    return Right(listPokemon);
   }
 }
---
+++ test/features/data/repositories/pokemon_repository_impl_test.dart
@@ -0,0 +1,79 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:pokedex_clean_arch/features/pokemon/data/datasources/remote_data_source.dart';
+import 'package:pokedex_clean_arch/features/pokemon/data/models/next_evolution_model.dart';
+import 'package:pokedex_clean_arch/features/pokemon/data/models/pokemon_model.dart';
+import 'package:pokedex_clean_arch/features/pokemon/data/repositories/pokemon_repository_impl.dart';
+import 'package:pokedex_clean_arch/features/pokemon/domain/entities/entities.dart';
+
+import 'pokemon_repository_impl_test.mocks.dart';
+
+@GenerateMocks([RemoteDataSource])
+void main() {
+  late MockRemoteDataSource mockRemoteDataSource;
+  late PokemonRepositoryImpl repository;
+
+  setUp(() {
+    mockRemoteDataSource = MockRemoteDataSource();
+    repository = PokemonRepositoryImpl(mockRemoteDataSource);
+  });
+
+  const tPokemonModel = PokemonModel(
+    id: 1,
+    num: '001',
+    name: 'Bulbasaur',
+    img: 'http://www.serebii.net/pokemongo/pokemon/001.png',
+    type: ['Grass', 'Poison'],
+    height: '0.71 m',
+    weight: '6.9 kg',
+    weaknesses: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    nextEvolution: [
+      NextEvolutionModel(
+        num: '002',
+        name: 'Ivysaur',
+      ),
+      NextEvolutionModel(
+        num: '003',
+        name: 'Venusaur',
+      )
+    ],
+  );
+
+  const tPokemon = Pokemon(
+    id: 1,
+    num: '001',
+    name: 'Bulbasaur',
+    img: 'http://www.serebii.net/pokemongo/pokemon/001.png',
+    types: ['Grass', 'Poison'],
+    height: '0.71 m',
+    weight: '6.9 kg',
+    weaknesses: ['Fire', 'Ice', 'Flying', 'Psychic'],
+    nextEvolution: [
+      NextEvolution(
+        img: '',
+        num: '002',
+        name: 'Ivysaur',
+      ),
+      NextEvolution(
+        img: '',
+        num: '003',
+        name: 'Venusaur',
+      ),
+    ],
+  );
+
+  group('get all pokemons', () {
+    test('should return all pokemons', () async {
+      //arrange
+      when(mockRemoteDataSource.getAllPokemons())
+          .thenAnswer((_) async => [tPokemonModel]);
+    });
+    //act
+    final result = repository.getAllPokemons();
+    //assert
+    verify(mockRemoteDataSource.getAllPokemons());
+    expect(result, equals(const Right(tPokemon)));
+  });
+}
