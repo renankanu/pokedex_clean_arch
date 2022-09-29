e0fc2629fe6bc99c8c55df8228a60c1ecdae2c9b
renankanu <renankanu@gmail.com>
2022-09-29 09:05:13 -0300

    refactor: Color scheme removed

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -5,6 +5,4 @@ final ThemeData baseTheme = ThemeData(
   primaryColor: Color(0xffffffff),
   primaryColorLight: Color(0xffe6e6e6),
   primaryColorDark: Color(0xff4d4d4d),
-  colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.white)
-      .copyWith(secondary: Color(0xff808080)),
 );
